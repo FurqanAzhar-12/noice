55a14a1c129881d7fde7d25f9a80f487eda50dca
VforVindictive <furqanazhar60@gmail.com>
2022-05-30 17:03:58 +0500

    commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const firstName=document.getElementById('fName');
+const lastName=document.getElementById('lName');
+const emailAddress=document.getElementById('eAddress');
+const button= document.getElementById('Register')
+const gender=document.getElementById('gender') 
+
+
+
+
+button.addEventListener('click',()=>{
+   { 
+  alert('Registration Successful')
+   }
+} )
