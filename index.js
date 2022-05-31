8fe1a6b80100f3ca8f251368d255592945cf47f0
VforVindictive <furqanazhar60@gmail.com>
2022-05-31 17:18:46 +0500

    commit

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
-const firstName=document.getElementById('fName');
-const lastName=document.getElementById('lName');
-const emailAddress=document.getElementById('eAddress');
-const button= document.getElementById('Register')
-const gender=document.getElementById('gender') 
+
+ var userName = document.getElementById('fName').value; console.log(userName);
+
+const button=document.getElementById('Register')
+
+button.addEventListener("click",function form(){
+   console.log("hello");
+
+   var userName = document.getElementById('fName').value; console.log(userName);
+   var userLastName = document.getElementById('lName').value;
+   var password= document.getElementById('password').value;
+   var cPassword= document.getElementById('cPassword').value;
+  // var mGender= document.getElementById('male');
+  // var fGender= document.getElementById('female');
+   var emailAddress=document.getElementById(eAddress);
+
+
+if (userName==='') {
+   alert('Enter Name')}
+  else if(userLastName===''||userLastName===null){
+         alert('Enter Last Name')}
+         else if(password===''||password!=cPassword){
+            alert('Invalid Password')}
+            /*else if(gender){
+               alert('Please Select Gender')*/
+            //}
+   else if(emailAddress===''||emailAddress===null){alert('Enter Email')}
+else{
+   alert('Registration Successful')
+}
+})
+         
 
 
 
+   
+       
+   
 
-button.addEventListener('click',()=>{
-   { 
-  alert('Registration Successful')
-   }
-} )
+
+   
