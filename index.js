a0a23d3974a1cf684c424534e60125077555257f
VforVindictive <furqanazhar60@gmail.com>
2022-06-01 15:55:26 +0500

    commit

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
 
- var userName = document.getElementById('fName').value; console.log(userName);
+
 
 const button=document.getElementById('Register')
 
-button.addEventListener("click",function form(){
-   console.log("hello");
 
+
+
+
+
+
+
+
+//Main FUnction
+
+button.addEventListener("click",function f()
+{
    var userName = document.getElementById('fName').value; console.log(userName);
    var userLastName = document.getElementById('lName').value;
    var password= document.getElementById('password').value;
    var cPassword= document.getElementById('cPassword').value;
-  // var mGender= document.getElementById('male');
-  // var fGender= document.getElementById('female');
-   var emailAddress=document.getElementById(eAddress);
+   var emailAddress=document.getElementById('eAddress').value;
+   var everything=['userLastName','userName','password','cPassword','emailAddress'];
+   //var gender= document.getElementByName('Gender');
+   
+  
 
+//Validation Process
+  
+if(userName==='') {
+      alert('Enter Name')}
+                           else if(userLastName===''||userLastName===null)
+                             {alert('Enter Last Name')}
+                           else if(password===''||password!=cPassword)   
+                              {alert('Invalid Password')}
+                           else if(emailAddress===''||emailAddress===null)
+                              {alert('Enter Email')}
+ else
+     {alert('Registration Successful')}
+           
+});
 
-if (userName==='') {
-   alert('Enter Name')}
-    else if(userLastName===''||userLastName===null){
-         alert('Enter Last Name')}
-         else if(password===''||password!=cPassword){
-            alert('Invalid Password')}
-            /*else if(gender){
-               alert('Please Select Gender')*/
-            //}
-            else if(emailAddress===''||emailAddress===null)
-            {alert('Enter Email')}
-else{
-   alert('Registration Successful')
-}
-})
          
 
-
-
+ 
    
        
    
