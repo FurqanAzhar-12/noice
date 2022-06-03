df8f2f45ec2cff5f5772b9dc80f17f7cca3980d6
VforVindictive <furqanazhar60@gmail.com>
2022-06-03 17:23:22 +0500

    commit

--- index.js
+++ index.js
@@ -2,41 +2,41 @@
 
 
 const button=document.getElementById('Register')
-
-
-
-
-
-
-
-
-
 //Main FUnction
 
 button.addEventListener("click",function f()
 {
-   var userName = document.getElementById('fName').value; console.log(userName);
+   var userName = document.getElementById('fName').value;
    var userLastName = document.getElementById('lName').value;
    var password= document.getElementById('password').value;
    var cPassword= document.getElementById('cPassword').value;
    var emailAddress=document.getElementById('eAddress').value;
-   var everything=['userLastName','userName','password','cPassword','emailAddress'];
-   //var gender= document.getElementByName('Gender');
-   
-  
+   var gender = '';
+   console.log(document.getElementById('Male').checked);
+   if(document.getElementById('Male').checked){
+      gender = document.getElementById('Male').value;
 
+   }else if(document.getElementById('Female').checked){
+      gender = document.getElementById('Female').value;
+   }
+  console.log(gender);
 //Validation Process
   
-if(userName==='') {
+if(userName=='') {
       alert('Enter Name')}
-                           else if(userLastName===''||userLastName===null)
+                           else if(userLastName==''||userLastName===null)
                              {alert('Enter Last Name')}
-                           else if(password===''||password!=cPassword)   
-                              {alert('Invalid Password')}
-                           else if(emailAddress===''||emailAddress===null)
-                              {alert('Enter Email')}
+                             else if(emailAddress==''||emailAddress===null)
+                             {alert('Enter Email')}
+                             else if(password==''||password!=cPassword)   
+                                {alert('Invalid Password')}
+                                else if(gender == ""){
+                                 alert('Plz select gender')
+                                }
  else
-     {alert('Registration Successful')}
+     {
+        alert('Registration Successful') 
+     }
            
 });
 
